5d6754740a34973a2b4e9d3e20df2a822f095dde
rajesh1979 <rajesh.basist@gmail.com>
2015-12-31 16:12:44 +0530

    updated

--- TestApplication/src/com/pojo/Employee.java
+++ TestApplication/src/com/pojo/Employee.java
@@ -5,4 +5,25 @@ public class Employee
 
 	private String name;
 	private String address;
+	private String email;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 }
