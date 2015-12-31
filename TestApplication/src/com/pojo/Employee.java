d0f76894132c39623ee233bf33c99d3f3223e9db
rajesh1979 <rajesh.basist@gmail.com>
2015-12-31 16:30:57 +0530

    updated

--- TestApplication/src/com/pojo/Employee.java
+++ TestApplication/src/com/pojo/Employee.java
@@ -7,7 +7,14 @@ public class Employee
 	private String address;
 	private String email;
 	private String phone;
+	private String profile;
 	
+	public String getProfile() {
+		return profile;
+	}
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
 	public String getPhone() {
 		return phone;
 	}
