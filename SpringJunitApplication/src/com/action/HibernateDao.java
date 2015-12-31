2582d454c536c7a1c8c799288e834b9d94f5b66d
rajesh1979 <rajesh.basist@gmail.com>
2015-12-31 16:40:58 +0530

    updated1

--- SpringJunitApplication/src/com/action/HibernateDao.java
+++ SpringJunitApplication/src/com/action/HibernateDao.java
@@ -1,5 +1,14 @@
 package com.action;
 
-public class HibernateDao {
+public class HibernateDao 
+{
 
+	public static void main(String args [])
+	
+	{
+		
+		System.out.println(">>>>>>>>>>>>>>>>>>>>");
+	}
+	
+	
 }
